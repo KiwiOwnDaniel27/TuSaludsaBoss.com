407c6c00b49245fae08debcfa99c78b5c74b9163
Daniel Quiguango <fiel2589@hotmail.com>
2026-01-20 22:35:41 -0500

    cambios ultimos

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter as Router } from "react-router-dom";
+
 import Header from './components/header';
 import Hero from './components/Hero';
 import Benefits from './components/benefits';
@@ -16,7 +18,6 @@ import 'aos/dist/aos.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className='container max-auto px-4'>
-        <Header/>
         <App />
         <Hero/>
         <Benefits/>
