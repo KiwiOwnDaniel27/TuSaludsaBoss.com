d98d3c00da4e6ff6b1a57b7d075328b5a66d85b3
Daniel Quiguango <fiel2589@hotmail.com>
2026-01-20 22:31:58 -0500

    cargar

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Header from './components/header';
+import Hero from './components/Hero';
+import Benefits from './components/benefits';
+import Planes from './components/Planes';
+import Testimonials from './components/Testimonials';
+import FaqSec from './components/FaqSec';
+import 'remixicon/fonts/remixicon.css';
+import 'animate.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div className='container max-auto px-4'>
+        <Header/>
+        <App />
+        <Hero/>
+        <Benefits/>
+        <Planes/>
+        <Testimonials/>
+        <FaqSec/>
+    </div>
   </StrictMode>,
-)
+);
