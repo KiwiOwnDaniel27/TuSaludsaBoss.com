c9fbfe15ffc4837102f19075c1da68319f9482cf
Daniel Quiguango <fiel2589@hotmail.com>
2026-01-20 23:19:17 -0500

    cargar ultimo cambio

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  homepage: "https://kiwiowndaniel27.github.io/TuSaludsaBoss.com",
+  base: "https://kiwiowndaniel27.github.io/TuSaludsaBoss.com/",
 })
