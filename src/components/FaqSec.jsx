422c110ad3224b5124beab69354e46af616f316c
Daniel Quiguango <fiel2589@hotmail.com>
2026-01-22 18:02:51 -0500

    Cambios finales link

--- src/components/FaqSec.jsx
+++ src/components/FaqSec.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import Titule from './Titule';
+import { faqimgs } from '../constant/data';
 
 const FaqSec = () => {
   return <section className="section">
     <div className="container">
         <div>
 
-        <Titule title='Preguntas Frecuentes' text='¿Aún tienes dudas sobre nuestros seguros médicos? Contacta a nuestro equipo a través de nuestro Whatsapp.' link='Ver Todas las PF'/>
+        <Titule title='Preguntas Frecuentes' 
+        text='¿Aún tienes dudas sobre nuestros seguros médicos? Contacta a nuestro equipo a través de nuestro Whatsapp.' 
+        link='Ver Todas las PF'/>
+
+        <div className="">
+        {faqimgs.map(item => (
+
+            <div className="">
+
+                <div className="">
+                    <h4 className="">
+                        
+                    </h4>
+                </div>
+            </div>
+        ))}
+        </div>
         </div>
     </div>
   </section>
